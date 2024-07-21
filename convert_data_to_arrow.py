99de5e446842ad312761804b210fcdf0ed29ad0c
William A Rodriguez <william.a@alum.mit.edu>
2024-07-21 15:50:06 +0100

    Created system prices arrow file

---
+++ convert_data_to_arrow.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from typing import List, Optional, Union
+
+import numpy as np
+from gluonts.dataset.arrow import ArrowWriter
+import pandas as pd
+
+def convert_to_arrow(
+    path: Union[str, Path],
+    time_series: Union[List[np.ndarray], np.ndarray],
+    start_times: Optional[Union[List[np.datetime64], np.ndarray]] = None,
+    compression: str = "lz4",
+):
+    if start_times is None:
+        # Set an arbitrary start time
+        start_times = [np.datetime64("2000-01-01 00:00", "s")] * len(time_series)
+
+    assert len(time_series) == len(start_times)
+
+    dataset = [
+        {"start": start, "target": ts} for ts, start in zip(time_series, start_times)
+    ]
+    ArrowWriter(compression=compression).write_to_file(
+        dataset,
+        path=path,
+    )
+
+if __name__ == "__main__":
+    # Generate 20 random time series of length 1024
+    time_series = [np.random.randn(1024) for i in range(20)]
+    # print(time_series)
+    print(np.shape(time_series))
+
+    # Convert to GluonTS arrow format
+    # convert_to_arrow("./noise-data.arrow", time_series=time_series)
+
+    # Load the dataset from csv
+    system_prices_df = pd.read_csv("./data/system_price.csv")
+    sytem_prices_values_np = np.array(system_prices_df["Daily average"].values)
+    print(sytem_prices_values_np)
+    print(sytem_prices_values_np)
+    print(np.shape([sytem_prices_values_np]))
+    convert_to_arrow("./system-prices.arrow", time_series=[sytem_prices_values_np])
