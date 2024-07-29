4083c76880b479c5b5d19aa53becab0ae8a18e62
AWS VM <william.a@alum.mit.edu>
2024-07-29 17:05:22 +0000

    chronos-tiny-336-48-8_000-delta

--- push_to_hugging_face.py
+++ push_to_hugging_face.py
@@ -1,5 +1,5 @@
 from chronos import ChronosPipeline
 
-pipeline = ChronosPipeline.from_pretrained("/home/ubuntu/chronos-forecasting-oracle/scripts/output/chronos-tiny-336-48-8_000-beta/checkpoint-final")
-pipeline.model.model.push_to_hub("chronos-tiny-336-48-8_000-beta")
+pipeline = ChronosPipeline.from_pretrained("/home/ubuntu/chronos-forecasting-oracle/scripts/output/chronos-tiny-336-48-8_000-delta/checkpoint-final")
+pipeline.model.model.push_to_hub("chronos-tiny-336-48-8_000-delta")
 print("Finished pusing model to HF")
